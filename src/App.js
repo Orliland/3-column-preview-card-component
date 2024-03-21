a7b34f955cfa3f94200baa4e334ddeac6ac47b4a
Orlando Verdin <git@orli.land>
2024-03-21 15:03:09 -0700

    add styles to cards

--- src/App.js
+++ src/App.js
@@ -9,23 +9,26 @@ const vehicles = {
     vehicleIcon: Sedans,
     vehicleDescription:
       "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+    vehicleColor: "orange",
   },
   suvs: {
     vehicleIcon: Suvs,
     vehicleDescription:
       "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+    vehicleColor: "teal",
   },
   luxury: {
     vehicleIcon: Luxury,
     vehicleDescription:
       "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style. ",
+    vehicleColor: "green",
   },
 };
 
 function VehicleCard({ model }) {
   const vehicle = vehicles[model];
   return (
-    <article className="vehicle">
+    <article className={`vehicle vehicle--${vehicle.vehicleColor}`}>
       <img
         src={vehicle.vehicleIcon}
         alt="Vehicle Icon"

4815d86f3383b6ff6dfc8d3708725660e9930c0f
Orlando Verdin <git@orli.land>
2024-03-21 12:33:21 -0700

    add vehicle class structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import "./App.css";
 
+import Sedans from "./assets/icon-sedans.svg";
+import Suvs from "./assets/icon-suvs.svg";
+import Luxury from "./assets/icon-luxury.svg";
+
+const vehicles = {
+  sedans: {
+    vehicleIcon: Sedans,
+    vehicleDescription:
+      "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+  },
+  suvs: {
+    vehicleIcon: Suvs,
+    vehicleDescription:
+      "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+  },
+  luxury: {
+    vehicleIcon: Luxury,
+    vehicleDescription:
+      "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style. ",
+  },
+};
+
 function VehicleCard({ model }) {
-  return <h2>{model}</h2>;
+  const vehicle = vehicles[model];
+  return (
+    <article className="vehicle">
+      <img
+        src={vehicle.vehicleIcon}
+        alt="Vehicle Icon"
+        className="vehicle__icon"
+      />
+      <h2 className="vehicle__name">{model.toUpperCase()}</h2>
+      <p className="vehicle__description">{vehicle.vehicleDescription}</p>
+      <button className="button">Learn More</button>
+    </article>
+  );
 }
 
 function Vehicles() {
